e2628a75cb21ee1350da88a3ed63131a6ac07c83
Polychronis Ypodimatopoulos <ypodim@gmail.com>
2019-12-29 19:57:24 -0800

    first attempt at deployable

---
+++ atlas/run_home.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# rc.local:
+# export HOUSE_PATH=/home/pi/ws/house/atlas
+# 
+
+
+export HOUSE_LOG="house.log"
+
+if [ -z ${HOUSE_PATH+x} ]; then echo "HOUSE_PATH var is unset, cannot continue" && exit 0; fi
+
+cd $HOUSE_PATH
+python3 main.py --log_file_prefix=$HOUSE_LOG
