66154377775ad80e686b03c5f5a981cb4a3824cc
Polychronis Ypodimatopoulos <ypodim@gmail.com>
2019-12-30 00:02:19 -0800

    RF refactored

--- atlas/homeAI.py
+++ atlas/homeAI.py
@@ -17,11 +17,9 @@ class homeAI(threading.Thread):
         self.log.info("light status is %s" % status)
 
         if status == "on":
-            self.log.info("turning lights off")
             self.rf.txCode(outletFamily, btn, "off")
             time.sleep(3)
         else:
-            self.log.info("turning lights on")
             self.rf.txCode(outletFamily, btn, "on")
             time.sleep(1)
 
