2a7acafd6df9d9a8b257a3760c3755583e73f78b
Polychronis Ypodimatopoulos <ypodim@gmail.com>
2020-01-05 22:38:33 -0800

    with ngrok

--- atlas/ngrok.py
+++ atlas/ngrok.py
@@ -6,13 +6,14 @@ class Ngrok(object):
         pass
     def getLocalInfo(self):
         req = urllib.request.Request(url="http://127.0.0.1:4040/api/tunnels")
-        with urllib.request.urlopen(req) as f:
-            data = json.loads(f.read().decode('utf-8'))
-            data = data.get("tunnels")
-            if not data:
-                return None
-            data = data[0]
-            return data.get("public_url")
+        try:
+            with urllib.request.urlopen(req) as f:
+                data = json.loads(f.read().decode('utf-8'))
+                return data.get("tunnels")[0].get("public_url")
+        except ConnectionRefusedError:
+            print("ConnectionRefusedError")
+            return None
+            
     def setup(self):
         pass
 
