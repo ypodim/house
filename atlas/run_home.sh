4816fe90afc4b1893635d1ca04dc03f3499f66e3
Polychronis Ypodimatopoulos <ypodim@gmail.com>
2020-01-11 22:53:50 -0800

    fix runtime

--- atlas/run_home.sh
+++ atlas/run_home.sh
@@ -8,6 +8,7 @@ export HOUSE_PATH=/home/pi/ws/house/atlas
 export HOUSE_LOG="house.log"
 
 if [ -z ${HOUSE_PATH+x} ]; then echo "HOUSE_PATH var is unset, cannot continue" && exit 0; fi
+if [ -z ${HOUSE_LOG+x} ];  then echo "HOUSE_LOG var is unset, cannot continue" && exit 0; fi
 
 cd $HOUSE_PATH
 python3 main.py --log_file_prefix=$HOUSE_PATH/$HOUSE_LOG
